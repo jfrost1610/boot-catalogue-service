ef3436fd25e2a8e1030914300dcaee89acff7d0d
jfrost1610 <jobinthomas1610@gmail.com>
2019-05-09 23:17:05 +0530

    Spring Boot basic setup

---
+++ src/main/java/com/frost/bootcatalogueservice/BootCatalogueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.frost.bootcatalogueservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCatalogueServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frost/bootcatalogueservice/BootCatalogueServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.frost.bootcatalogueservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootCatalogueServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
